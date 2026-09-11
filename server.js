c6456b8009ae7ee62215c5051981c547605a4eaa
medhanshbot90-tech <medhanshbot90@gmail.com>
2026-09-11 15:51:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(__dirname, {
+  index: false
+}));
 
 // ================= API KEYS =================
 
